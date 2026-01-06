83600f1bb1f96c23b5d179d460f64f5d1ac41dde
Konstantin Rybakov <k.ribakov@gmail.com>
2026-01-06 22:34:34 +0200

    Added EKS module and Helm-chart for django project

--- outputs.tf
+++ outputs.tf
@@ -47,3 +47,20 @@ output "aws_nat_gateway_id" {
   description = "ID NAT Gateway"
   value       = module.vpc.aws_nat_gateway_id
 }
+
+#-------------EKS-----------------
+
+output "eks_cluster_endpoint" {
+  description = "EKS API endpoint for connecting to the cluster"
+  value       = module.eks.eks_cluster_endpoint
+}
+
+output "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.eks_cluster_name
+}
+
+output "eks_node_role_arn" {
+  description = "IAM role ARN for EKS Worker Nodes"
+  value       = module.eks.eks_node_role_arn
+}
